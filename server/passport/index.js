8b2e82edf08da8580b41b061dbb49b32a3e2fa70
Sahil Ali <sahil.sheikh@jda.com>
2019-12-06 05:05:57 +0530

    The whole passort thingy

--- server/passport/index.js
+++ server/passport/index.js
@@ -1,39 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('./passportConfig');
 
+const setAuthRouter = function (router) {
+  router.use(passport.initialize())
+  router.use(passport.session())
+  // path to start the OAuth flow
+  router.get('/api/slack/auth', passport.authorize('slack'))
 
-    const randomBearerToken = "abcxyz";
-    let isLoggedIn = false
-    router.get('/api/login', function(req,res) {
-        if(req.session.token) {
-            res.sendStatus(200);
-        } else {
-            req.session.payload = {access_token: randomBearerToken};
-            console.log(req.session)
-            res.sendStatus(201)
-        }
-    });
-    router.get('/api/isLoggedIn', function(req,res) {
-      //  const isLoggedIn = Math.random() >= 0.5;
-      console.log(req.session);
-        if(req.session && req.session.payload) {
-            const {access_token} = req.session.payload;
-            res.status(200);
-            res.json({isLoggedIn:true, access_token})
-        } else {
-            res.sendStatus(401)
-        }
-    });
-    router.get('/api/logout' , function(req,res) {
-        req.session.destroy(function(err) {
-            console.log(err);
-            if(err) {
-                res.sendStatus(500)
-            } else {
-                res.sendStatus(200)
-            }
-        });
-    })
-
-
-module.exports = router;
+  // OAuth callback url
+  router.get(
+    '/api/slack/callback',
+    passport.authenticate('slack', { failureRedirect: '/login' }),
+    (req, res) => {
+      const { user } = req
+     res.redirect('/api/slack/isLoggedIn')
+    }
+  )
+  router.get('/api/slack/isLoggedIn', function (req, res) {
+    //  const isLoggedIn = Math.random() >= 0.5;
+    console.log(req.user)
+    console.log(req.session)
+    if (req.user) {
+      const { user } = req
+      res.status(200)
+      res.json({ isLoggedIn: true, user })
+    } else {
+      res.sendStatus(401)
+    }
+  })
+  return router
+}
+module.exports = setAuthRouter
