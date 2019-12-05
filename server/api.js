04a522a27a26b5687fc895d8b83556ae7343ede1
Sahil Ali <sahil.sheikh@jda.com>
2019-12-06 05:05:16 +0530

    added two boiler plate api files that can be used with express

--- server/api.js
+++ server/api.js
@@ -1,36 +1,7 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const session = require('express-session')
-const FileStore = require('session-file-store')(session);
-const cookieParser = require('cookie-parser');
-
-const router = express.Router()
-
-
-// const hour = 3600000;
-// const fileStoreOptions = {}
-// const appFileStore = new FileStore(fileStoreOptions)
-// const appSession = session({
-//   store: appFileStore,
-//   secret: 'abc123',
-//   resave: false,
-//   saveUninitialized: true,
-//   maxAge: hour,
-//   cookie: { secure: true }
-// })
-
-
-// router.use(bodyParser.json());
-// router.use(cookieParser());
-// router.use(appSession)
-
-// API CONTROLLERS HERE ( ONLY PUBLIC)
-// var authAPILayer = require('./passport/index');
-// authAPILayer(router);
-
-router.get('/api/test', function (req, res) {
-  res.status(200)
-  res.json({ message: 'I am ok' })
-})
-
-module.exports = router
+const routes = function (app) {
+  app.get('/api/route1/test', function (req, res) {
+    res.status(200)
+    res.json({ message: 'I am ok' })
+  })
+}
+module.exports = routes
