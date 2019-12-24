91a133b15920af558c7a5bc8e19e986c1d62c645
Pratik Kumar <pratik.kumar@lsz.ads.valuelabs.net>
2019-12-24 16:58:22 +0530

    correction made

--- src/dashboard/Homepage.js
+++ src/dashboard/Homepage.js
@@ -2,20 +2,20 @@ import React from 'react';
 export const Homepage = () => {
     return (
         <>
-            <header class="v-header container">
-                <div class="fullscreen-video-wrap">
+            <header className="v-header container">
+                <div className="fullscreen-video-wrap">
                     <video loop autoPlay>
                         <source src='../../static/background.mov' type="video/mp4" />
                     </video>
                 </div>
-                <div class="header-overlay"></div>
-                <div class="header-content text-md-center">
+                <div className="header-overlay"></div>
+                <div className="header-content text-md-center">
                     <h1>Welcome Everyone</h1>
 
                     <hr></hr>
                     <p>Any app that can be written in JavaScript, will eventually be written in JavaScript. <pre className="quote-owner"> Jeff Atwood</pre></p>
                    
-                    <a class="btn">Find Out More</a>
+                    <a className="btn">Find Out More</a>
                 </div>
             </header>
         </>
